a3f8ad901c8afcc061554111c5e7bcfc742c8f28
pongelupe <pongelupe1998@gmail.com>
2023-12-12 14:56:20 -0300

    final version manuscript and presentation

---
+++ manuscript/pos-texto/codigo.c
@@ -0,0 +1,17 @@
+/*
+	Soma dois numeros inteiros.
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b, c;
+	printf("a: ");
+	scanf("%d", &a);
+	printf("b: ");
+	scanf("%d", &b);
+	c = a + b;
+	printf("a + b = %d\n", c);
+	return 0;
+}
